4bd12f43462b6753a85571c0f7db76705cade7f8
oycyc <yangciouou@gmail.com>
2025-05-24 08:45:29 -0400

    use try catch then

--- dist/index.js
+++ dist/index.js
@@ -26214,15 +26214,21 @@ function formatResults(results, coverageResults, showCoverage) {
         let row = `| ${testFileName} | ${statusText} | ${result.passed} | ${result.total} `;
         if (showCoverage) {
             let coverageText = "N/A";
-            let uncoveredLinesDetails = "";
-            if (coverageInfo) {
-                coverageText = `${coverageInfo.coverage.toFixed(2)}%`;
-                if (coverageInfo.notCoveredLines &&
-                    coverageInfo.notCoveredLines !== "N/A") {
-                    uncoveredLinesDetails = `<details><summary>Uncovered Lines</summary>${coverageInfo.notCoveredLines}</details>`;
+            try {
+                let uncoveredLinesDetails = "";
+                if (coverageInfo) {
+                    coverageText = `${coverageInfo.coverage.toFixed(2)}%`;
+                    if (coverageInfo.notCoveredLines &&
+                        coverageInfo.notCoveredLines !== "N/A") {
+                        uncoveredLinesDetails = `<details><summary>Uncovered Lines</summary>${coverageInfo.notCoveredLines}</details>`;
+                    }
                 }
+                row += `| ${coverageText} ${uncoveredLinesDetails} `;
             }
-            row += `| ${coverageText} ${uncoveredLinesDetails} `;
+            catch (error) {
+                console.error("Error processing coverage information:", error);
+                console.log("Coverage Info:", coverageInfo);
+            }
         }
         row += `| ${detailsColumn} |\n`;
         output += row;
